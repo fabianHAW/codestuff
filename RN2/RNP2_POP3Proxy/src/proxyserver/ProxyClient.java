4619200e593e6d472fbc9c88002355b70af7d1d1
Francis Opoku <Francis.Opoku@haw-hamburg.de>
2014-11-01 18:36:36 +0100

    #Changes: Server: Passwort entfernt ProxyClient: Bugfixes

--- RN2/RNP2_POP3Proxy/src/proxyserver/ProxyClient.java
+++ RN2/RNP2_POP3Proxy/src/proxyserver/ProxyClient.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import javax.net.ssl.HandshakeCompletedEvent;
+import javax.net.ssl.HandshakeCompletedListener;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 import proxyserver.exceptions.InvalidNumberException;
 import proxyserver.exceptions.InvalidPasswordException;
@@ -37,15 +43,16 @@ public class ProxyClient extends Thread{
 	
 	public ProxyClient(){
 		//this.connection = connection;
-		TIMEINTERVAL = 30000;
+		TIMEINTERVAL = 2000;
 		this.accountList = new ArrayList<POP3Account>();
 		commands = new Command();
 	}
 	
 	public ProxyClient(List<POP3Account> a){
 		//this.connection = connection;
-		TIMEINTERVAL = 30000;
+		TIMEINTERVAL = 2000;
 		this.accountList = a;
+		commands = new Command();
 	}
 	
 	/**
@@ -61,30 +68,54 @@ public class ProxyClient extends Thread{
 			e.printStackTrace();
 		}
 		
-		
-		try {
-			fetchMails( POP3Proxy.getKnownAccounts() );
-		} catch (InvalidUserException e) {
-			System.out.println("ProxyClient.run: fetchMails throws InvalidUserException.");
-			e.printStackTrace();
-		} catch (InvalidPasswordException e) {
-			System.out.println("ProxyClient.run: fetchMails throws InvalidPasswordException.");
-			e.printStackTrace();
-		} catch (InvalidNumberException e) {
-			System.out.println("ProxyClient.run: fetchMails throws InvalidNumberException.");
-			e.printStackTrace();
+		List<POP3Account> accounts = POP3Proxy.getKnownAccounts();
+		if (! (accounts.size() == 0)) {
+			try {
+				fetchMails(POP3Proxy.getKnownAccounts());
+			} catch (InvalidUserException e) {
+				System.out
+						.println("ProxyClient.run: fetchMails throws InvalidUserException.");
+				e.printStackTrace();
+			} catch (InvalidPasswordException e) {
+				System.out
+						.println("ProxyClient.run: fetchMails throws InvalidPasswordException.");
+				e.printStackTrace();
+			} catch (InvalidNumberException e) {
+				System.out
+						.println("ProxyClient.run: fetchMails throws InvalidNumberException.");
+				e.printStackTrace();
+			}
 		}
-		
 		//loop(every 30secs) do fuer jeden account holen
 			//loop(i to accountList.length) authentifizieren und mails holen
 				//loop(j to numberOfMail) alle vorhandenen Mails vom Server holen und speichern
 	}
 	
 	public void fetchMails(List<POP3Account> accounts) throws InvalidUserException, InvalidPasswordException, InvalidNumberException{
+		SSLSocketFactory sslSocketFactory = (SSLSocketFactory)SSLSocketFactory.getDefault();
 		for(POP3Account a : accounts){
 			try {
 				//Socket zu Mailserver einrichten
-				Socket connectionToMailserver= new Socket(a.getAddress(), a.getPort());
+				SSLSocket connectionToMailserver= (SSLSocket)sslSocketFactory.createSocket(a.getAddress(), a.getPort());
+			
+				connectionToMailserver.setUseClientMode(true);
+				connectionToMailserver.setEnabledProtocols(connectionToMailserver.getSupportedProtocols());
+				
+				String[] suites = connectionToMailserver.getSupportedCipherSuites();
+				connectionToMailserver.setEnabledCipherSuites(suites);
+				connectionToMailserver.addHandshakeCompletedListener(
+						new HandshakeCompletedListener() {
+							
+							@Override
+							public void handshakeCompleted(HandshakeCompletedEvent event) {
+								System.out.println("Handshake abgeschlossen.");
+								System.out.println("Verwendete cipher suite: " + event.getCipherSuite());
+								
+							}
+				});
+				connectionToMailserver.startHandshake();
+				//connectionToMailserver.setKeepAlive(true);
+				System.out.println("getKeepAlive " + connectionToMailserver.getKeepAlive());
 				
 				//reader, writer zum Mailserver
 				PrintWriter writer = new PrintWriter(new OutputStreamWriter(
@@ -93,7 +124,7 @@ public class ProxyClient extends Thread{
 						connectionToMailserver.getInputStream(), StandardCharsets.UTF_8.name()));
 				
 				//Alle Mails des Accounts anfordern.
-				List<Email> mails = getMails(reader, writer, a);
+				List<Email> mails = getMails(connectionToMailserver,reader, writer, a);
 		
 				connectionToMailserver.close();
 				
@@ -107,29 +138,53 @@ public class ProxyClient extends Thread{
 		}
 	}
 	
-	public List<Email> getMails(Scanner reader, PrintWriter writer, POP3Account account) throws InvalidUserException, InvalidPasswordException, InvalidNumberException{
+	public List<Email> getMails(SSLSocket connection, Scanner reader, PrintWriter writer, POP3Account account) throws InvalidUserException, InvalidPasswordException, InvalidNumberException{
 		List<Email> mails = new ArrayList<Email>();
 		List<String> tmpResponseOfListCommand = new ArrayList<String>();
 		String response;
 		
 		//User Authentifikation
-		writer.write(commands.user(account.getUser()));
+		/*
+		System.out.println("getMails:writer.checkError: " + writer.checkError());
+		System.out.println("accountData: " + account.getPass() + " " + account.getUser() + " " + account.getServeraddress());
+		System.out.println("commands.user(): " + commands.user(account.getUser()));
+		*/
 		response = reader.nextLine();
-		if(! response.matches("^(+OK)*")){
+		System.out.println("response1: " + response);
+		
+		writer.println(commands.user(account.getUser()));
+		
+		/*
+		System.out.println("" + connection.isConnected());
+		System.out.println("" + connection.isInputShutdown());
+		System.out.println("" + connection.isOutputShutdown());
+		System.out.println("" + connection.isBound());
+		System.out.println("" + connection.isClosed());
+		System.out.println(reader.ioException());
+		System.out.println("after write1");
+		*/
+		
+		response = reader.nextLine();
+		System.out.println("response2: " + response);
+		if(! response.matches("\\+OK.*")){
 			throw new InvalidUserException("ProxyClient.getMails: Benutzername falsch.");
 		}
 		
 		//Passwort Authentifikation
-		writer.write(commands.pass(account.getPass()));
+		writer.println(commands.pass(account.getPass()));
 		response = reader.nextLine();
-		if(! response.matches("^(+OK)*")){
+		System.out.println("response3: " + response);
+		if(! response.matches("\\+OK.*")){
 			throw new InvalidPasswordException("ProxyClient.getMails: Passwort falsch.");
 		}
 		
 		//LIST Kommando senden, List-Response in Liste speichern
-		writer.write(commands.list());
-		while(reader.hasNextLine()){
-			tmpResponseOfListCommand.add(reader.nextLine());
+		writer.println(commands.list());
+		reader.nextLine();
+		while(!response.equals(".") && reader.hasNextLine()){
+			response = reader.nextLine();
+			System.out.println("responsex: " + response);
+			tmpResponseOfListCommand.add(response);
 		}
 		
 		int bytesize;
@@ -138,35 +193,44 @@ public class ProxyClient extends Thread{
 		String[] splitMessage;
 		//Auswertung LIST Kommando, ggf. Mails empfangen und speichern.
 		for(int i = 0; i < tmpResponseOfListCommand.size(); i++){
+			System.out.println("i: " + i);
 			splitMessage = tmpResponseOfListCommand.get(i).split(" ", 3); //3 = 1 zu viel?
+			for(int j = 0; j < tmpResponseOfListCommand.size(); j++){
+				System.out.println("tmpResponse...:" + tmpResponseOfListCommand.get(j));
+			}
+			System.out.println("" + splitMessage[0] + " " + splitMessage[1]);
 			bytesize = Integer.parseInt(splitMessage[1]);
 			tmpNr = Integer.parseInt(splitMessage[0]);
-			
+			System.out.println("Integer: " + tmpNr);
 			//UIDL anfordern / TODO: Errorhandling - noMail?
-			writer.write(commands.uidl(tmpNr));
+			writer.println(commands.uidl(tmpNr));
+			System.out.println(commands.uidl(tmpNr));
 			response = reader.nextLine();
+			System.out.println("response5: " + response);
 			splitMessage = response.split(" ", 3);
 			uidl = splitMessage[2];
 			
 			//Mail anfordern / TODO: Errorhandling - noMail?
-			writer.write(commands.retr(tmpNr));
+			writer.println(commands.retr(tmpNr));
 			response = reader.nextLine(); //Erste Antwort uberspringen (+OK n octets)
+			System.out.println("response6: " + response);
 			response = "";
-			while(reader.hasNextLine() && !response.equals(".")){
+			while(!response.equals(".") && reader.hasNextLine()){
 				response += reader.nextLine();
+				//System.out.println("responsex2: " + response);
 			}
-			
+			System.out.println("after " + i);
 			Email mail = new Email(response, bytesize, uidl);
-			
+			System.out.println("after "  + i);
 			//Empfangene Mail loeschen.
-			writer.write(commands.dele(tmpNr));
-			
+			writer.println(commands.dele(tmpNr));
+			System.out.println("after "  + i);
 			mails.add(mail);
-			
+			System.out.println("after "  + i);
 		}
-		
-		writer.write(commands.quit());
-		
+		System.out.println("after end");
+		writer.println(commands.quit());
+		System.out.println("after end 2");
 		return mails;
 	}
 	/**
