67982da61f4f3ed272d27f9cf7e1a5424e8ea217
fabianHAW <fabian.reiber@haw-hamburg.de>
2014-11-26 17:54:45 +0100

    berechnung der rtt und der neuen timeouts nach rfc2988 und karn-algorithmus (s. wiki -> tcp 4.4 retransmission timer) eingebaut | logfile wird erstellt und ist mit allen, laut aufgabenblatt, windowsizes und errorrates durchgeführt (allerdings auf localhost) | der alte code den wir zusammen geschrieben haben, ist in dem ordner "RNP3_FiletransferApp_old_14-11-26_1638" zu finden. | sequenzdiagram folgt in kuerze :)

---
+++ RN3/RNP3_FiletransferApp_old_14-11-26_1638/src/client/TestClass.java
@@ -0,0 +1,20 @@
+package client;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println(Math.round(4.3d));
+		System.out.println(Math.round(4.5d));
+		System.out.println(Math.round(4.6d));
+
+		System.out.println((int)Math.ceil(4.3d));
+		System.out.println((int)Math.ceil(4.5d));
+		System.out.println((int)Math.ceil(4.6d));
+		
+		System.out.println(7 / 3);
+		
+		System.out.println(Math.ceil(840439 / 1000));
+
+	}
+
+}
