3a6e1ce19a8ebf06d35f3646c1a74666747f22dc
fabianHAW <fabian.reiber@haw-hamburg.de>
2015-04-03 11:12:09 +0200

    dlq ist nun fertig

--- VS/VSP1/dlq.erl
+++ VS/VSP1/dlq.erl
@@ -2,12 +2,12 @@
 -export([initDLQ/2, expectedNr/1, push2DLQ/3, deliverMSG/4]).
 -import(werkzeug, [timeMilliSecond/0, logging/2]).
 
-
 %Initialisieren der DLQ
 %return ist 2-Tupel mit 1. Element Size und 2. Element leere DLQ-Liste
 initDLQ(Size, Datei) ->
 	logging(Datei, lists:flatten(io_lib:format("DLQ >> initialisiert mit Kapazitaet: ~p~n", [Size]))),
-	{Size, [[2,test,1,2,3],[3,hallo,1,2,3],[4,hallo,1,2,3]]}.
+	{Size, []}.
+	%{Size, [[2,test,1,2,3],[3,hallo,1,2,3],[4,hallo,1,2,3], [6,hallo,1,2,3]]}.
 	
 %Liefert die Nachrichtennummer, die als naechstes gespeichert werden kann
 %ist die Liste leer, wird 1 zurueck gegeben
@@ -15,7 +15,6 @@ expectedNr({_Size, []}) ->
 	1;
 expectedNr({_Size, [Head | Tail]}) ->
 	expectedNr(Head, Tail).
-
 %Hilfsmethode die die DLQ  rekursiv durchlaeuft bis sie am Ende
 %der Liste angekommen ist. Gibt dann die NNr + 1 zurueck.
 expectedNr([NNr, _Msg, _TSclientout, _TShbqin, _TSdlqin], []) ->
@@ -32,7 +31,6 @@ push2DLQ([NNr, Msg, TSclientout, TShbqin], {Size, Queue}, Datei) ->
 	MsgNew = lists:flatten(io_lib:format("~p TSdlqin: " ++ MSGTimestamp, [Msg])),
 	logging(Datei, lists:flatten(io_lib:format("DLQ >> Nachricht ~p in DLQ eingefuegt~n", [NNr]))),
 	push2DLQ([NNr, MsgNew, TSclientout, TShbqin], TSdlqin, Size, length(Queue), Queue, Datei).
-
 %Wurd die maximale Groesse der DLQ erreicht, wird das aelteste Element (vorne) geleoscht
 push2DLQ(List, TSdlqin, Size, Length, [[NNr, _Msg, _TSclientout, _TShbqin, _TSdlqinOld] | Tail], Datei) when Length == Size -> 
 	logging(Datei, lists:flatten(io_lib:format("DLQ >> Nachricht ~p aus DLQ geloescht~n", [NNr]))),
@@ -41,7 +39,18 @@ push2DLQ(List, TSdlqin, Size, _Length, Queue, _Datei) ->
 	ListNew = List ++ [TSdlqin],
 	{Size, Queue ++ [ListNew]}.
 	
-	
-%TODO
-deliverMSG(MSGNr, CientPID, Queue, Datei) ->
-	ok.
+%liefert eine Nachricht an den Client aus und gibt der HBQ die gesendete Nachrichtennummer zurueck
+%hier wurde das Ende der zur Verfuegung stehenden Nachrichten erreicht und dem
+%Client signalisiert, dass es keine weiteren Nachrichten mehr gibt, Terminated = true
+deliverMSG(MSGNr, ClientPID, {_Size, [[NNr, Msg, TSclientout, TShbqin, TSdlqin] | []]}, Datei) when MSGNr =< NNr ->
+	ClientPID ! {reply, [NNr, Msg, TSclientout, TShbqin, TSdlqin] ++ [erlang:now()], true},
+	logging(Datei, lists:flatten(io_lib:format("DLQ >> Nachricht ~p an Client ~p ausgeliefert~n", [NNr, ClientPID]))),
+	NNr;
+%es gibt noch weitere Nachrichten die dem Client auf Anfrage zugestellt werden koennen, Terminated = false
+deliverMSG(MSGNr, ClientPID, {_Size, [[NNr, Msg, TSclientout, TShbqin, TSdlqin] | _Tail]}, Datei) when MSGNr =< NNr ->
+	ClientPID ! {reply, [NNr, Msg, TSclientout, TShbqin, TSdlqin] ++ [erlang:now()], false},
+	logging(Datei, lists:flatten(io_lib:format("DLQ >> Nachricht ~p an Client ~p ausgeliefert~n", [NNr, ClientPID]))),
+	NNr;
+%rekursiver Durchlauf durch die Liste
+deliverMSG(MSGNr, ClientPID, {Size, [[_NNr, _Msg, _TSclientout, _TShbqin, _TSdlqin] | Tail]}, Datei) ->
+	deliverMSG(MSGNr, ClientPID, {Size, Tail}, Datei).
