dafb31b5bb920acddb1ac19eca3ba8fa8195f0a7
Francis Opoku <Francis.Opoku@haw-hamburg.de>
2015-04-26 18:17:57 +0200

    Koordinator im createRing + Hilfsmethoden erweitert. Nicht getestet.

--- VS/VSP2/koordinator.erl
+++ VS/VSP2/koordinator.erl
@@ -1,5 +1,5 @@
 -module(koordinator).
--import(werkzeug, [get_config_value/2, logging/2, timeMilliSecond/0]).
+-import(werkzeug, [get_config_value/2, logging/2, timeMilliSecond/0, shuffle/1]).
 -export([start/0]).
 -define(LOGFILE, lists:flatten(io_lib:format("~p.log", [node()]))).
 
@@ -44,13 +44,89 @@ start() ->
 			logging(?LOGFILE, lists:flatten(io_lib:format("Koordinator: Erfolgreich an Nameservice gebunden. ~n", [])))
 	end,
 	
-	loop(Arbeitszeit, Termzeit, Ggtprozessnummer, Nameservicenode, Nameservicename, Koordinatorname, Quote, Korrigieren, PIDns)
+	loop(Arbeitszeit, Termzeit, Ggtprozessnummer, Nameservicenode, Nameservicename, Koordinatorname, Quote, Korrigieren, PIDns, [], undef)
 	.
-		
-loop(AZ, TZ, GGTPNr, NameSn, Nameservicename, Koordinatorname, Quote, Korrigieren, PIDns) ->
-	print("loop Noch nicht implementiert! ~n")
+%AZ := Arbeitszeit; TZ := Termzeit; GGTPNr := GGT-Prozessnummer; NameSno := NameserviceNode
+%NameSna := NameserviceName; KN := Koordinatorname; QUO := Quote; KOR := Korrigieren
+%PIDns := PID-Nameservice; CMD := Command; GGTL := GGT-Prozessliste
+loop(AZ, TZ, GGTPNr, NameSno, NameSna, KN, QUO, KOR, PIDns, GGTL, CMD) when CMD /= step ->
+	receive
+	%%%%********************************************************************************************************************%%%%
+	%%%%*****************************************Initialiserungsphase*******************************************************%%%%
+	%%%%********************************************************************************************************************%%%%
+		{StarterPID, getsteeringval} ->
+			Quota = round(QUO / 100) * 1, %ToDo: Anzahl erwarteter Prozesse statt 1
+			StarterPID ! {steeringval, AZ, TZ, Quota, GGTPNr},
+			loop(AZ, TZ, GGTPNr, NameSno, NameSna, KN, QUO, KOR, PIDns, GGTL, CMD);
+		{hello, GGTName} ->
+			loop(AZ, TZ, GGTPNr, NameSno, NameSna, KN, QUO, KOR, PIDns, GGTL ++ GGTName, CMD);
+		{step} ->
+			loop(AZ, TZ, GGTPNr, NameSno, NameSna, KN, QUO, KOR, PIDns, createRing(GGTL, PIDns), step);
+	%%%%********************************************************************************************************************%%%%
+	%%%%*****************************************Arbeitsphase***************************************************************%%%%
+	%%%%********************************************************************************************************************%%%%
+	
+	%%%%********************************************************************************************************************%%%%
+	%%%%*****************************************Beendigungsphase***********************************************************%%%%
+	%%%%********************************************************************************************************************%%%%
+		{reset} ->
+			print("reset nicht implementiert ~n");
+		{prompt} ->
+			print("prompt nicht implementiert ~n");
+		{nudge} ->
+			print("nudge nicht implementiert ~n");
+		{toggle} ->
+			print("toggle nicht implementiert ~n");
+		{kill} ->
+			print("kill nicht implementiert ~n")
+	end	
 	.
 	
+createRing(GGTL, PIDns) ->
+	createRing(werkzeug:shuffle(GGTL), 1, PIDns),
+	GGTL.
+createRing(GGTL, Idx, PIDns) ->
+	PIDns ! {self(), {lookup, lists:nth(length(GGTL))}},
+	receive
+		{pin, {GGTName, GGTNode}} ->
+			{GGTName, GGTNode} ! {setneighbors,getLneighbor(Idx, PIDns, GGTL), getRneighbor(Idx, PIDns, GGTL)},
+			createRing(GGTL, Idx + 1, PIDns)
+	end
+	.
+
+getRneighbor(Idx, PIDns, GGTL) when Idx == length(GGTL) ->
+	PIDns ! {self(), {lookup, lists:nth(1, GGTL)}},
+	receive
+		{pin, {GGTName, GGTNode}} ->
+		Rneighbor = {GGTName, GGTNode}
+	end,
+	Rneighbor
+	;
+getRneighbor(Idx, PIDns, GGTL) ->
+	PIDns ! {self(), {lookup, lists:nth(Idx + 1, GGTL)}},
+	receive
+		{pin, {GGTName, GGTNode}} ->
+		Rneighbor = {GGTName, GGTNode}
+	end,
+	Rneighbor
+	.
+	
+getLneighbor(1, PIDns, GGTL) when  length(GGTL) > 1 ->
+	PIDns ! {self(), {lookup, lists:nth(length(GGTL), GGTL)}},
+	receive
+		{pin, {GGTName, GGTNode}} ->
+		Lneighbor = {GGTName, GGTNode}
+	end,
+	Lneighbor
+	;
+getLneighbor(Idx, PIDns, GGTL) ->
+	PIDns ! {self(), {lookup, lists:nth(Idx - 1, GGTL)}},
+	receive
+		{pin, {GGTName, GGTNode}} ->
+		Lneighbor = {GGTName, GGTNode}
+	end,
+	Lneighbor
+	.
 	
 readConfig() ->
 	{ok, CfgList} = file:consult("koordinator.cfg"),
