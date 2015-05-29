46389e4a2ae2a25278a0dcdc0d5e237b24afbb9a
Francis Opoku <Francis.Opoku@haw-hamburg.de>
2015-05-29 19:33:48 +0200

    Thread umgestrickt

--- VS/VSP3/src/mware_lib/CommunicationModuleThread.java
+++ VS/VSP3/src/mware_lib/CommunicationModuleThread.java
@@ -1,7 +1,9 @@
 package mware_lib;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
@@ -9,23 +11,27 @@ import java.net.Socket;
  * @author Francis und Fabian
  * 
  *         Dieser Thread wird vom Proxy (Client-Seite) erzeugt. Der Proxy teilt
- *         ihm mit welche Nachricht er Ã¼ber das Netzwerk auf die Server-Seite
+ *         ihm mit welche Nachricht er über das Netzwerk auf die Server-Seite
  *         versenden muss. Danach wartet der jeweilige Thread auf die Antwort.
  *         Trifft diese beim Kommunikationsmodul ein, so wird der Thread geweckt
- *         und kann dem Proxy das Ergebnis (MessageADT) zurÃ¼ck liefern.
+ *         und kann dem Proxy das Ergebnis (MessageADT) zurück liefern.
  */
 public class CommunicationModuleThread extends Thread {
 
 	private MessageADT sendMessage;
 	private MessageADT receivedMessage;
+	private ServerSocket serversocket;
 	private Socket socket;
 	private ObjectOutputStream output;
+	private ObjectInputStream input;
 
 	public CommunicationModuleThread(MessageADT m) {
 		CommunicationModule.debugPrint(this.getClass(), "initialized");
 		this.sendMessage = m;
 		try {
-
+			
+			this.serversocket = new ServerSocket(0);
+			this.sendMessage.setport(this.serversocket.getLocalPort());
 			this.socket = new Socket(m.getObjectRef().getInetAddress(), m
 					.getObjectRef().getPort());
 
@@ -45,14 +51,19 @@ public class CommunicationModuleThread extends Thread {
 
 				CommunicationModule.debugPrint(this.getClass(),
 						"waiting for reply of own communication module");
-				this.wait();
-			} catch (IOException e) {
+				//this.wait();
+				this.socket = this.serversocket.accept();
+				this.input = new ObjectInputStream(this.socket.getInputStream());
+				this.setReceivedMessage((MessageADT) this.input.readObject());
+				
+			} catch (IOException | ClassNotFoundException e) {
 				e.printStackTrace();
-			} catch (InterruptedException e) {
-				CommunicationModule.debugPrint(this.getClass(),
-						"someone interrupted me");
-				notify();
 			}
+//			} catch (InterruptedException e) {
+//				CommunicationModule.debugPrint(this.getClass(),
+//						"someone interrupted me");
+//				notify();
+//			}
 		}
 		CommunicationModule.removeThreadFromList(this);
 	}
