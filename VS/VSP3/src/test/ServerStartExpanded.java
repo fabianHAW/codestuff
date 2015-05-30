310ee144a689d185a0bc5578e1a66c4b5dc71bf6
Francis Opoku <Francis.Opoku@haw-hamburg.de>
2015-05-30 12:12:12 +0200

    Nebenläufigkeitstests eingebaut

---
+++ VS/VSP3/src/test/ServerStartExpanded.java
@@ -0,0 +1,75 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import mware_lib.NameService;
+import mware_lib.ObjectBroker;
+import accessor_one.ClassOneAO;
+import accessor_one.ClassTwoAO;
+import accessor_two.ClassOneAT;
+
+public class ServerStartExpanded extends Thread{
+
+	private int nameservicePort;
+	private String nameserviceHost;
+	private ArrayList<String> servants;
+
+	public ServerStartExpanded(int nsport, String nshost) {
+		// TODO Auto-generated constructor stub
+		servants = new ArrayList<String>();
+		nameservicePort = nsport;
+		nameserviceHost = nshost;
+	}
+	
+	public void run(){
+		accessor_one_two_test();
+	}
+	
+	public void accessor_one_two_test(){
+		ObjectBroker objBroker = ObjectBroker.init(this.nameserviceHost, this.nameservicePort, true);
+		NameService nameSvc = objBroker.getNameService();
+	
+		
+		Random classid = new Random();
+		ArrayList<Integer> counts = new ArrayList<Integer>();
+		counts.add(classid.nextInt(10)); //counts(0) = ClassOneAO
+		counts.add(classid.nextInt(10)); //counts(1) = ClassTwoAO
+		counts.add(classid.nextInt(10)); //counts(2) = ClassOneAT
+		
+		//Generates a random number of servants
+		for(int i = 0; i < counts.get(0); i++){
+			String name = "1_classOneAO-" + i;
+			nameSvc.rebind(new ClassOneAO(), name);
+			servants.add(name);
+		}
+		
+		for(int i = 0; i < counts.get(1); i++){
+			String name = "2_classTwoAO-" + i;
+			nameSvc.rebind(new ClassTwoAO(), name);
+			servants.add(name);
+		}
+		
+		for(int i = 0; i < counts.get(1); i++){
+			String name = "3_classOneAT-" + i;
+			nameSvc.rebind(new ClassOneAT(), name);
+			servants.add(name);
+		}
+
+		
+		try {
+			System.out.println("server: sleeping");
+			Thread.sleep(40000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		objBroker.shutDown();
+		System.out.println("end server");
+	}
+	
+	public ArrayList<String> getServantNames(){
+		return servants;
+	}
+
+}
---
+++ VS/VSP3/src/test/SmallConcurrencyTest.java
@@ -0,0 +1,65 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SmallConcurrencyTest {
+
+	public SmallConcurrencyTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		if (args.length != 4) {
+			usage();
+			System.exit(-1);
+		}
+		
+		int nsport = Integer.parseInt(args[0]);
+		String nshost = args[1];
+		
+		int servercount = Integer.parseInt(args[2]);
+		int clientcount = Integer.parseInt(args[3]);
+		
+		ArrayList<ServerStartExpanded> server = startServer(nshost, nsport, servercount);
+		ArrayList<ClientStartExpanded> clients = startClients(nshost, nsport, clientcount, server);
+		
+		start(server, clients);
+		
+	}
+	
+	public static void start(ArrayList<ServerStartExpanded> server, ArrayList<ClientStartExpanded> clients){
+		for(ServerStartExpanded s : server){
+			s.start();
+		}
+		for(ClientStartExpanded c : clients){
+			c.start();
+		}
+	}
+	
+	public static ArrayList<ServerStartExpanded> startServer(String nshost, int nsport, int servercount){
+		ArrayList<ServerStartExpanded> server = new ArrayList<ServerStartExpanded>();
+		for(int i = 0; i < servercount; i++){
+			server.add(new ServerStartExpanded(nsport, nshost));
+		}
+		return server;
+	}
+	
+	public static ArrayList<ClientStartExpanded> startClients(String nshost, int nsport, int clientcount, ArrayList<ServerStartExpanded> servers){
+		ArrayList<ClientStartExpanded> clients = new ArrayList<ClientStartExpanded>();
+		Random rand = new Random();
+		for(int i = 0; i < clientcount; i++){
+			clients.add(new ClientStartExpanded(nsport, nshost, servers.get(rand.nextInt(servers.size())).getServantNames(), 5));
+		}
+		return clients;
+	}
+	
+	private static void usage() {
+		System.out
+				.println("SmallConcurrencyTest java/SmallConcurrencyTest <nameservice-port> <nameservice-host> <number-of-servers> <number-of-clients>");
+	}
+	
+
+}
