0f12e3d5a1df7be47c8ca3b626f7eee597dee4db
fabianHAW <fabian.reiber@haw-hamburg.de>
2015-06-11 12:25:17 +0200

    stations-test erweitert

--- VS/VSP4/station1.erl
+++ VS/VSP4/station1.erl
@@ -1,8 +1,30 @@
 -module(station1).
--export([start/0]).
+-export([start/1, startA/1]).
 
-start() ->
-	Socket = werkzeug:openSe({127,0,0,1}, 50000),
-	gen_udp:controlling_process(Socket, self()),
-	timer:apply_after(2000, gen_udp, close, [Socket]),
-	gen_udp:send(Socket, localhost, 50001, "hallo").
+-define(MULTICASTADDRESS, {127,0,0,1}).
+-define(OWNADDRESS, {127,0,0,2}).
+-define(MULTICASTPORT, 15001).
+-define(OWNPORT, 15002).
+-define(TIMETOSLEEP, 1000).
+
+start(0) ->
+	finished;
+start(Counter) ->
+	Socket = werkzeug:openSe(?OWNADDRESS, ?OWNPORT),
+	%gen_udp:controlling_process(Socket, self()),
+	%timer:apply_after(2000, gen_udp, close, [Socket]),
+	gen_udp:send(Socket, ?MULTICASTADDRESS, ?MULTICASTPORT, "hallo"),
+	timer:sleep(?TIMETOSLEEP),
+	gen_udp:close(Socket),
+	start(Counter - 1).
+
+startA(0) ->
+	finished;
+startA(Counter) ->
+	Socket = werkzeug:openSeA(?OWNADDRESS, ?OWNPORT),
+	%gen_udp:controlling_process(Socket, self()),
+	%timer:apply_after(2000, gen_udp, close, [Socket]),
+	gen_udp:send(Socket, ?MULTICASTADDRESS, ?MULTICASTPORT, "hallo"),
+	timer:sleep(?TIMETOSLEEP),
+	gen_udp:close(Socket),
+	startA(Counter - 1).
--- VS/VSP4/station2.erl
+++ VS/VSP4/station2.erl
@@ -1,8 +1,30 @@
 -module(station2).
--export([start/0]).
+-export([start/1, startA/1]).
 
-start() ->
-	Socket = werkzeug:openRec({127,0,0,2}, {127,0,0,1}, 50001),
+-define(MULTICASTADDRESS, {127,0,0,1}).
+-define(OWNADDRESS, {127,0,0,2}).
+-define(MULTICASTPORT, 15001).
+
+start(0) ->
+	finished;
+start(Counter) ->
+	Socket = werkzeug:openRec(?MULTICASTADDRESS, ?OWNADDRESS, ?MULTICASTPORT),
 	gen_udp:controlling_process(Socket, self()),
-	timer:apply_after(2000, gen_udp, close, [Socket]),
-	gen_udp:recv(Socket, 0).
+	%timer:apply_after(2000, gen_udp, close, [Socket]),
+	Result = gen_udp:recv(Socket, 0),
+	io:format("Result: ~p~n", [Result]),
+	gen_udp:close(Socket),
+	start(Counter - 1).
+
+startA(0) ->
+	finished;
+startA(Counter) ->
+	Socket = werkzeug:openRecA(?MULTICASTADDRESS, ?OWNADDRESS, ?MULTICASTPORT),
+	gen_udp:controlling_process(Socket, self()),
+	%timer:apply_after(2000, gen_udp, close, [Socket]),
+	receive
+	 Result ->
+		io:format("Result: ~p~n", [Result])
+	end,
+	gen_udp:close(Socket),
+	startA(Counter - 1).
