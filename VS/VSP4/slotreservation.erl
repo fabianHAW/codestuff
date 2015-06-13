4ae01e92fb37fc6bd6712a097a4517c2ef98368d
Francis Opoku <Francis.Opoku@haw-hamburg.de>
2015-06-13 21:32:41 +0200

    Slotreservation implementiert

--- VS/VSP4/slotreservation.erl
+++ VS/VSP4/slotreservation.erl
@@ -7,11 +7,50 @@
 
 start(SenderPID) ->
 	SenderPID ! {helloSlot, self()},
-	debug("send own pid to sender", ?DEBUG).
+	debug("send own pid to sender", ?DEBUG),
+	loop([], SenderPID)
+.
 
+%Antwortet auf Anfragen von MessageGen und Sender
+loop(FreeSlots, SenderPID) ->
+	receive
+		{slot, NextSlot} ->
+			loop(lists:append(FreeSlots, [NextSlot]), SenderPID);
+		{getSlot, MessageGenPID} ->
+			{FreeSlotsNew, NextSlot} = getNewSlot(FreeSlots),
+			MessageGenPID ! {nextSlot, NextSlot},
+			loop(FreeSlotsNew, SenderPID);
+		{collision, Slot} ->
+			sendCollisionAnswer(FreeSlots, Slot, SenderPID),
+			loop(FreeSlots, SenderPID);
+		kill ->
+			kill()
+	end
+.
 
-getNewSlot() ->
-	ok.
+%Sendet den nächsten freien Slot an MessageGen oder {nextSlot, nok},
+%wenn kein freier Slot zur Verfügung steht.
+getNewSlot([]) ->
+	{[], {nextSlot, nok}};
+getNewSlot([Next | Rest]) ->
+	{Rest, Next}
+.
+
+%Sendet {collision, true} an den Sender, wenn der Slot in dem
+%der Sender sende möchte, nicht in der Liste der freien Slots ist,
+%sonst {collision, false}
+sendCollisionAnswer([], _Slot, SenderPID) ->
+	SenderPID ! {collision, true};
+sendCollisionAnswer([First | _Rest], Slot, SenderPID) when First == Slot->
+	SenderPID ! {collision, false};
+sendCollisionAnswer([_First | Rest], Slot, SenderPID) ->
+	sendCollisionAnswer(Rest, Slot, SenderPID)
+.	
 
 debug(Text, true) ->
 	io:format("slotreservation_module: ~p~n", [Text]).
+
+%TODO: Log
+kill() ->
+	ok
+.
