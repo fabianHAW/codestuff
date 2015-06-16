3a341fea28572032a384e31bed02c2dddd69f1eb
abp491 <abp491@hathi.informatik.haw-hamburg.de>
2015-06-16 13:29:19 +0200

    kommentar entfernt

--- VS/VSP4/timesync.erl
+++ VS/VSP4/timesync.erl
@@ -47,7 +47,6 @@ accurate(UtcOffsetMs, SyncOffsetMs, TimesReceived) ->
 			SyncOffsetNew = berkley(string:equal(StationClass, "A"), TimeInSlot, SyncOffsetMs, TimesReceived + 1),
 			accurate(UtcOffsetMs, SyncOffsetNew, TimesReceived + 1);
 		{getTime, SenderPID_MessageGenPID} ->
-			io:format("~p ~p~n", [SyncOffsetMs, UtcOffsetMs]),
 			SenderPID_MessageGenPID ! {currentTime, SyncOffsetMs + UtcOffsetMs}, %TODO: SyncOffset Berechnen Berkley
 			accurate(UtcOffsetMs, SyncOffsetMs, TimesReceived);
 		kill ->
