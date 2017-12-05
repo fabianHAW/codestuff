6166fb5595b4fc8278df98f4960180f9b93e9c2b
Xaaris <xaaris@googlemail.com>
2017-12-05 22:59:46 +0100

    Update cointoss.py

--- uo-wise17/cointoss.py
+++ uo-wise17/cointoss.py
@@ -1,8 +1,9 @@
 import random
 
 
-def toss_coin(num_flips, filename):
+def toss_coin(num_flips, cointoss_data_filename, cointoss_history_filename):
     flips = []
+    balance = []
     counter_heads = 0
     counter_heads_temp = 0
     counter_tails = 0
@@ -20,22 +21,30 @@ def toss_coin(num_flips, filename):
                 counter_heads = max(counter_heads, counter_heads_temp)
                 counter_heads_temp = 0
                 counter_tails_temps += 1
+        balance.append(counter_heads_temp - counter_tails_temps)
     counter_heads = max(counter_heads, counter_heads_temp)
     counter_tails = max(counter_tails, counter_tails_temps)
     heads = sum([x == 'Heads' for x in flips])
     tails = num_flips - heads
-    with open(filename, 'a') as f:
+    with open(cointoss_data_filename, 'a') as f:
         f.write('{};{};{};{};{};{}\n'.format(
             heads, tails, counter_heads, counter_tails,
             heads - tails, tails - heads))
+    with open(cointoss_history_filename, 'a') as f:
+        for flip in range(num_flips):
+            f.write('{};'.format(balance[flip]))
+        f.write('\n')
 
 
 if __name__ == '__main__':
-    rounds = 1000
-    num_flips = 100000
-    filename = 'coin_toss_{}_2.csv'.format(rounds)
-    with open(filename, 'w') as f:
+    rounds = 5
+    num_flips = 1000
+    cointoss_data_filename = 'coin_toss_data_{}.csv'.format(rounds)
+    cointoss_history_filename = 'coin_toss_history_{}.csv'.format(rounds)
+    with open(cointoss_data_filename, 'w') as f:
         f.write('#heads;#tails;max heads series;max tails series;euro heads;'
                 'euro tails\n')
+    with open(cointoss_history_filename, 'w') as f:
+        f.write('Coin toss balance history\n')
     for _ in range(rounds):
-        toss_coin(num_flips, filename)
+        toss_coin(num_flips, cointoss_data_filename, cointoss_history_filename)
